15f932c7fd6753cb51ef36fef74590ddd53fd544
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-08-09 17:51:53 -0400

    multer settings

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,6 +4,7 @@ const upload = require('../multer-file');
 const User = require('../models/user');
 const Post = require('../models/post');
 
+// create new post - user from token, caption and image from form
 exports.create_post = [
   upload.single('form-image'),
   body('form-caption').trim().escape(),
@@ -13,20 +14,21 @@ exports.create_post = [
 
     const { user } = req;
     const caption = req.body['form-caption'];
-    const image = req.file.filename;
+    const image = req.file.key;
     const date = Date.now();
 
     const post = new Post({
       user, caption, image, date,
     });
 
-    post.save((err) => {
+    post.save((err, newPost) => {
       if (err) res.json(err);
-      res.json('new post!');
+      res.json(newPost._id);
     });
   },
 ];
 
+// update caption
 exports.update_post = [
   body('form-caption').trim().escape(),
   (req, res, next) => {
@@ -34,9 +36,10 @@ exports.update_post = [
     if (!errors.isEmpty()) return res.json({ errors: errors.array });
 
     const update = { caption: req.body['form-caption'] };
-    Post.findByIdAndUpdate(req.params.id, update, (err, updatedPost) => {
+    console.log(update);
+    Post.findByIdAndUpdate(req.params.id, update, {}, (err, updatedPost) => {
       if (err) res.json(err);
-      res.json('post caption update');
+      res.json(updatedPost._id);
     });
   },
 ];
--- multer-file.js
+++ multer-file.js
@@ -1,28 +1,32 @@
-const aws = require('aws-sdk');
+const { S3Client } = require('@aws-sdk/client-s3');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 const { MulterError } = multer;
 
-aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  accessKeyId: process.env.AWS_KEY_ID,
+// s3 settings and credentials
+const s3 = new S3Client({
   region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+  },
+  sslEnabled: false,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
 });
-const s3 = new aws.S3();
 
+// storage bucket and image filename for key
 const storage = multerS3({
   s3,
-  acl: 'public-read',
-  bucket: 'finstagram-images',
+  bucket: process.env.AWS_BUCKET,
   key(req, file, cb) {
     cb(null, Date.now() + file.originalname);
   },
 });
 
+// file type has to be image
 const fileFilter = (req, file, cb) => {
   if (!file.mimetype.includes('image/')) {
     return cb(new MulterError('file has to be an image'), false);
