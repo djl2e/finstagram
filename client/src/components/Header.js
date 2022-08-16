8b9d3676bc544a0242535b2e963fa6d034fe5c86
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-08-16 14:24:13 -0400

    web header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Header(props) {
+  const navigate = useNavigate();
+
+  if (props.user == null) {
+    return;
+  } 
+
+  const userId = props.user._id;
+
+  function signout() {
+    localStorage.setItem('token', JSON.stringify(null));
+    props.setUser(undefined);
+    props.setIsLoggedIn(false);
+    navigate('/auth/login');
+  }
+
   return (
     <div className="header">
       <Link to="/">Finstagram</Link>
@@ -9,7 +24,8 @@ function Header(props) {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/posts/create">New Post</Link>
-        <Link to="/users/profile">Profile</Link>
+        <Link to={"/users/" + userId}>Profile</Link>
+        <button onClick={signout}>Log Out</button>
       </nav>
     </div>
   )
