73e7670e15b9d08a2a522dd20898c02951b86ff4
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-08-16 14:23:50 -0400

    change user info forms

---
+++ client/src/components/RemoveUser.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom'
+
+function RemoveUser(props) {
+  const navigate = useNavigate();
+
+  function submitDelete(e) {
+    e.preventDefault();
+    const config = {
+      headers: { 
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+      }
+    }
+    axios
+      .post('/users/delete', {}, config)
+      .then((res) => {
+        localStorage.setItem('token', JSON.stringify(null));
+        props.setUser(undefined);
+        props.setIsLoggedIn(false);
+        navigate('/auth/login');
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  return (
+    <form className="remove-user-form" onSubmit={submitDelete}>
+      <p>Delete User: {props.user.username}?</p>
+      <button type="submit">Yes, Delete</button>
+      <Link to={`/users/${props.user._id}`}>No, Return Back to Profile</Link>
+    </form>
+  )
+}
+
+export default RemoveUser;
