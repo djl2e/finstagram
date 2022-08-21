ea9bc0c4fdef341cea6638f798a3b03bea56a916
Daniel Joseph Lee <danieljlee96@gmail.com>
2022-08-21 17:33:55 -0400

    styling header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import '../style/Header.css'
 
 function Header(props) {
   const navigate = useNavigate();
+  const imgSrc = 'https://finstagram-images.s3.us-east-1.amazonaws.com/';
 
   if (props.user == null) {
     return;
@@ -19,12 +21,18 @@ function Header(props) {
 
   return (
     <div className="header">
-      <Link to="/">Finstagram</Link>
-      <input type="text" placeholder="search for username"/>
+      <Link to="/" id="logo">Finstagram</Link>
+      <input type="text" placeholder="search for username" id="search-bar"/>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/posts/create">New Post</Link>
-        <Link to={"/users/" + userId}>Profile</Link>
+        <Link to="/">
+          <img src={imgSrc + 'home.png'} alt="home" />
+        </Link>
+        <Link to="/posts/create">
+          <img src={imgSrc + 'add.png'} alt="new post" />
+        </Link>
+        <Link to={"/users/" + userId}>
+          <img src={imgSrc + 'user.png'} alt="profile" />
+        </Link>
         <button onClick={signout}>Log Out</button>
       </nav>
     </div>
